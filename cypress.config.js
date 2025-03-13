41f43d45ee2abaec5179dd451a9bd2edb762cf49
Bruno Matheus Ribeiro <brunomatheus168@gmail.com>
2025-03-13 20:41:28 -0300

    "Configure cypress cloud"

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "met3aj",
   e2e: {
+    
     // viewportHeight: 880,
     // viewportWidth: 1280,
     setupNodeEvents(on, config) {
